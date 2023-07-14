cabd909bc7c690fe9182a303326c1aa69b17b86d
JohnLuke Thermos <102494674+jlthermos28@users.noreply.github.com>
2023-07-14 16:01:40 -0400

    stuff

--- postdeploy.py
+++ postdeploy.py
@@ -9,7 +9,59 @@ def postdeploywindow():
         postdeploy = Tk()
         postdeploy.title("Post Deploy Template")
         postdeploy.geometry("1000x500")
+        
+        #pre logic variable for the pre logic question
+        neworreplace = 1 #1 is new, 2 is replacement
+        lapordesk = 1 #1 is laptop, 2 is desktop
+        
+        
+        
+        
+        #-------FUNCTIONS THAT DEPENDS ON CONDITIONALS-------#
+        
+        
+        #new machine function
+        
+        
+        
+        
+        #replacement function
+        
+        
+        
+        
+        
+        
+        
+        #-------INPUT FIELDS-------# 
+        
+        
+        
+        
 
+        
+        
+        
+        #------CONDITIONALS------#
+        
+        #runs new machine function
+        
+        
+        #runs replacement machine function
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         mainloop()
 
 
